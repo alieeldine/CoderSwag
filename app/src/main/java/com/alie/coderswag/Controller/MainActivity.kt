d41dbe37c3ab18873740e23757e548e456df3a10
alie el dine <alieeldine@gmail.com>
2019-06-06 11:31:16 +0200

    lesson 01

--- app/src/main/java/com/alie/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/alie/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alie.coderswag
+package com.alie.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.alie.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
